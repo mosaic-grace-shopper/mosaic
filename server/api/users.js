be6c2d11337ad30574950ed0bf37d484dbccb4c2
johanna fulghum <johanna.fulghum@gmail.com>
2018-02-26 17:31:46 -0500

    working on updating to admin

--- server/api/users.js
+++ server/api/users.js
@@ -13,7 +13,27 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
-router.delete('/:id', (req, res, next) =>{
+router.put('/:id/admin', (req, res, next) => {
+  User.update({
+    isAdmin: true
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).spread((numberOfRows, affectedRows) => {
+    res.json(affectedRows)
+  })
+  .catch(next)
+})
+
+router.get('/:id', (req, res, next) => {
+  User.findById(req.params.id)
+  .then(user => res.json(user))
+  .catch(next)
+})
+
+
+router.delete('/:id', (req, res, next) => {
   User.destroy({
     where: {
       id: req.params.id
