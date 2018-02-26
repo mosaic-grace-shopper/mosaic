8688ae799fe27643c5546dc3cfb3efbc0d639eb9
Roxie Turner <roxie.turner@gmail.com>
2018-02-26 18:06:21 -0500

    Mostly done with model, complete association and add routes

---
+++ server/db/models/product.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Product = db.define('product', {
+  artist: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: 'Untitled'
+  },
+  description: {
+    type: Sequelize.TEXT,
+    // Maybe add charachter minimum?
+  },
+  price: {
+   type: Sequelize.DECIMAL,
+   allowNull: false,
+   defaultValue: 0,
+   validate: {
+     min: 0,
+   }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+  imgUrl: {
+   type: Sequelize.STRING,
+   allowNull: false,
+   defaultValue: 'https://c2.staticflickr.com/8/7742/17504602910_92edbff02f_b.jpg',
+   //maybe think about custom validation for images?
+  }
+});
+
+module.exports = Product;
