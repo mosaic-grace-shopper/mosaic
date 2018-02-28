86e2eb693bd0f5c723ec1960932045fcea21fee2
Annabel Lau <annabel.n.lau@gmail.com>
2018-02-28 14:12:26 -0500

    formatting

--- client/components/single-product.js
+++ client/components/single-product.js
@@ -3,7 +3,6 @@ import {connect} from 'react-redux'
 import {withRouter, Link} from 'react-router-dom'
 
 function SingleProduct(props){
-    console.log(props.singleProduct, "single PRODUCTTTT")
     const product = props.singleProduct
     if (!product) return <div />
     return (
