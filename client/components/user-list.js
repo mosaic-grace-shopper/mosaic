2e986286d5bbf2c216ec3c3e1aea90181b4dad87
johanna fulghum <johanna.fulghum@gmail.com>
2018-02-28 15:42:03 -0500

    All Users!

--- client/components/user-list.js
+++ client/components/user-list.js
@@ -1,34 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { allUsersThunk } from '../store/users';
+import React from "react";
+import { connect } from "react-redux";
+import { allUsersThunk } from "../store/users";
 
-const UserList = (props) => {
+const UserList = props => {
+  return (
+    <div>
+      <ul>{props.users.map(user => <li key={user.id}>{user.email} </li>)}</ul>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <ul>
-            {props.users.map(user => 
-                <li key={user.id}>{user.email} </li>
-            )}
-            </ul>
-        </div>
-    )
-}
-
-
-const mapStateToProps = function(state){
-   return {
-       users: state.users
-   }
-}
+const mapStateToProps = function(state) {
+  return {
+    users: state.users
+  };
+};
 
 const mapDispatch = dispatch => {
-    return {
-        getAllUsers() {
-            dispatch(allUsersThunk())
-        }
+  return {
+    getAllUsers() {
+      dispatch(allUsersThunk());
     }
   };
-
+};
 
 export default connect(mapStateToProps, mapDispatch)(UserList);
