fadc98bb72d2daad624582ad30903ab31fc94400
johanna fulghum <johanna.fulghum@gmail.com>
2018-03-01 15:35:13 -0500

    You can only see users if you are logged in

---
+++ server/api/utils.js
@@ -0,0 +1,22 @@
+const makeError = (status, message) => {
+    const err = new Error(message);
+    err.status = status;
+    return err;
+}
+const isAdmin = (req, res, next) => {
+    console.log("req", req)
+    if (!req.user.isAdmin) return next(makeError(403, 'Forbidden'))
+    next()
+}
+
+const isLoggedIn = (req, res, next)=> {
+	if (!req.user) return next(makeError(401, 'Login'))
+	next()
+}
+
+
+module.exports = {
+    isAdmin,
+    isLoggedIn,
+    makeError
+}
