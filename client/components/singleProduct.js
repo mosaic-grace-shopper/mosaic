45e898275449cdad95a80bdabb86892a1001e322
Roxie Turner <roxie.turner@gmail.com>
2018-03-02 12:03:58 -0500

    cart almost happening

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Link} from 'react-router-dom'
+import { updateCartThunk } from '../store/cart';
 
+
+// probably needs to be a stateful component?
 function SingleProduct( props ){
     const product = props.singleProduct
+
+
+    // function handleSubmit(evt) {
+    //     evt.preventDefault();
+    //     console.log('Form submit clicked');
+    //     console.log('Ordering ', evt.target.quantity.value, ' of', product.title, ' id no:', product.id);
+    //     // check to see if there is a cart on the state?
+    //     // send put with props.id to /api/cart
+
+    // }
+
     if (!product) return <div />
     return (
         <div>
@@ -13,7 +27,10 @@ function SingleProduct( props ){
             <h4>{product.description}</h4>
             <h4>${product.price}</h4>
             <h4><em>{product.quantity} available</em></h4>
-            <button>Buy this Item</button>
+            <form onSubmit={props.handleSubmit}>
+                <input type="number" name="quantity" step="1" defaultValue="1" min="1" max={product.quantity} />
+                <button>Buy this Item</button>
+            </form>
             <button><Link to="/products">Back to Products</Link></button>
         </div>
     )
@@ -25,5 +42,18 @@ const mapState = function(state, ownProps){
     }
 }
 
-export default withRouter(connect(mapState, null)(SingleProduct))
+const mapDispatch = function(dispatch, ownProps) {
+    return {
+        handleSubmit(evt) {
+            evt.preventDefault();
+            const newId = ownProps.match.params.id;
+            const orderLine = {
+                newId: +evt.target.quantity.value
+            }
+            dispatch(updateCartThunk(orderLine))
+        }
+    }
+}
+
+export default withRouter(connect(mapState, mapDispatch)(SingleProduct))
 
