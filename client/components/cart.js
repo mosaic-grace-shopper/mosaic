72b065b5a2a36b4c8002ec3a97b46878878b0dd2
Annabel Lau <annabel.n.lau@gmail.com>
2018-03-03 17:49:42 -0500

    formatting

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
-import {Checkout} from './checkout'
+import { Checkout } from './checkout'
 import { getCartThunk, updateCartThunk, deleteCartThunk } from '../store'
 
 
@@ -21,34 +21,34 @@ class Cart extends Component {
 
         return (
             <div>
-            <div>
-            <h1>My Cart</h1>
-            {
-                products.filter(product => cartItems.includes(String(product.id))).map(filteredProduct => (
-                    <ul key={filteredProduct.id}>
-                    <li key={filteredProduct.id}><h3><em>{filteredProduct.title}</em> by {filteredProduct.artist}</h3>
-                    <h4>
-                    Quantity: {cart[filteredProduct.id]}
-                    <form onSubmit={this.props.handleSubmit}>
-                    <input type="hidden" name="id" value={filteredProduct.id} readOnly />
-                    <input type="number" name="quantity" step="1" defaultValue={cart[filteredProduct.id]} min="0" />
-                    <button>update quantity</button>
-                    </form>
-                    </h4>
-                    <h4>Unit Price: ${filteredProduct.price} </h4>
-                    
-                    <h4>Price: $
+                <div>
+                    <h1>My Cart</h1>
+                    {
+                        products.filter(product => cartItems.includes(String(product.id))).map(filteredProduct => (
+                            <ul key={filteredProduct.id}>
+                                <li key={filteredProduct.id}><h3><em>{filteredProduct.title}</em> by {filteredProduct.artist}</h3>
+                                    <h4>
+                                        Quantity: {cart[filteredProduct.id]}
+                                        <form onSubmit={this.props.handleSubmit}>
+                                            <input type="hidden" name="id" value={filteredProduct.id} readOnly />
+                                            <input type="number" name="quantity" step="1" defaultValue={cart[filteredProduct.id]} min="0" />
+                                            <button>update quantity</button>
+                                        </form>
+                                    </h4>
+                                    <h4>Unit Price: ${filteredProduct.price} </h4>
+
+                                    <h4>Price: $
                     {filteredProduct.price * cart[filteredProduct.id]}
-                    </h4>
-                    </li>
-                    </ul>
-                )
-            )
-        }
-        <h1>Total: </h1>
+                                    </h4>
+                                </li>
+                            </ul>
+                        )
+                        )
+                    }
+                    <h1>Total: </h1>
                     <button onClick={this.props.handleClick}>Empty your cart</button>
-                    </div>
-                    <Link to="/products"><button>Back to Products</button></Link>
+                </div>
+                <Link to="/products"><button>Back to Products</button></Link>
             </div>
         )
     }
