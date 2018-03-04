de0bdc8d42194bce7c7b4986cbdd972411a50c02
johanna fulghum <johanna.fulghum@gmail.com>
2018-03-04 18:42:22 -0500

    delete products if you are admin works!

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { Product, Review } = require('../db/models')
+const { isAdmin, isLoggedIn } = require('./utils')
 
 router.get('/', (req, res, next) => {
     Product.findAll()
@@ -14,7 +15,7 @@ router.post('/', (req, res, next) => {
 })
 
 router.get('/:id', (req, res, next) => {
-    Product.findOne(req.params.id)
+    Product.findById(req.params.id)
         .then(product => res.json(product))
         .catch(next)
 })
@@ -29,31 +30,54 @@ router.get('/reviews/:id', (req, res, next) => {
         .catch(next)
 })
 
-
-
 router.put('/:id', (req, res, next) => {
-    Product.update(req.body, {
-        where: {
-            id: req.params.id
-        },
-        returning: true
-    }).then(([num, updatedProduct]) => {
-        num ? res.json(updatedProduct[0]) : res.status(404).send()
-    })
-        .catch(next)
-})
+    Product.findById(req.params.id)
+      .then(product => product.update(req.body))
+      .then(updatedProduct => res.json(updatedProduct))
+      .catch(next)
+  })
 
-router.delete(':/id', (req, res, next) => {
-    Product.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-        .then(rowDeleted => {
-            rowDeleted ? res.status(205).send()
-                : res.status(404).send();
-        })
-        .catch(next)
-})
+
+// router.put('/:id', (req, res, next) => {
+//     Product.update(req.body, {
+//         where: {
+//             id: req.params.id
+//         },
+//         returning: true
+//     }).then(([num, updatedProduct]) => {
+//         num ? res.json(updatedProduct[0]) : res.status(404).send()
+//     })
+//         .catch(next)
+// })
+
+// router.delete(':/id', (req, res, next) => {
+//     Product.destroy({
+//         where: {
+//             id: req.params.id
+//         }
+//     })
+//         .then(numRowsDeleted => {
+//             console.log(numRowsDeleted)
+//         })
+//         .catch(next)
+// })
+
+
+//this works!! 
+// router.delete('/:id',  (req, res, next) => {
+//     Product.destroy({where: { id: req.params.id } })
+//       .then(() => res.sendStatus(202))
+//       .catch(next)
+//   })
+
+  router.delete('/:id', isAdmin, (req, res, next) => {
+    let isAdmin = req.user !== undefined ? req.user.isAdmin : false ;
+    let query = isAdmin ? {where: { id: req.params.id } } : {} 
+    Product.destroy(query)
+      .then(() => res.sendStatus(202))
+      .catch(next)
+  })
+  
+  
 
 module.exports = router
