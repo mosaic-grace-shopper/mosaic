fef4e87105d0bc6981ab93cc7093e2da7fc79049
Roxie Turner <roxie.turner@gmail.com>
2018-03-06 17:36:44 -0500

    some logic for updating quantity post checkout

--- client/components/checkout.js
+++ client/components/checkout.js
@@ -4,7 +4,23 @@ import { withRouter, Link } from 'react-router-dom'
 import {addNewShipmentDetailsThunk} from '../store'
 
 function Checkout(props) {
+
     console.log(props.cart, "CARTTTT")
+    console.log(props.products, "PRODUCTS 4 UPDATE")
+    const cartKey = Object.keys(props.cart);
+    const prodsBefore = props.products;
+    const prodsAfter = [];
+
+    for (let i = 0; i < prodsBefore.length; i++) {
+        console.log(cartKey, "is cart key");
+        console.log(String(prodsBefore[i].id), "is prodsBefore id");
+        if (cartKey.includes(String(prodsBefore[i].id))) {
+            console.log('I am doing it');
+            prodsAfter.push(prodsBefore[i])
+        }
+    }
+    console.log(prodsAfter);
+
     return (
         <div>
             <h1>Checkout</h1>
@@ -52,7 +68,8 @@ function Checkout(props) {
 
 const mapState = function(state){
     return {
-        cart: state.cart
+        cart: state.cart,
+        products: state.products
     }
 }
 
