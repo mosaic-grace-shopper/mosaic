82c75c6ccbc95d14cb73025bf054fdaab78676a9
johanna fulghum <johanna.fulghum@gmail.com>
2018-03-06 16:36:31 -0500

    renders

--- client/components/productList.js
+++ client/components/productList.js
@@ -1,46 +1,9 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { allProducts } from "../store";
-import RaisedButton from "material-ui/RaisedButton";
-import FontIcon from "material-ui/FontIcon";
-import {
-  Card,
-  CardActions,
-  CardHeader,
-  CardMedia,
-  CardTitle,
-  CardText
-} from "material-ui/Card";
-import ProductItem from "./productItem";
-import NewProductForm from "./newProduct";
-
-const styles = {
-  button: {
-    margin: 12
-  },
-  card: {
-    maxWidth: 345
-  },
-  media: {
-    height: 200
-  },
-  exampleImageInput: {
-    cursor: "pointer",
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    width: "100%",
-    opacity: 0
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "space-around"
-  }
-};
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { allProducts } from '../store';
+import ProductItem from './productItem';
+import NewProductForm from './newProduct';
 
 class ProductList extends Component {
   componentDidMount() {
@@ -54,21 +17,21 @@ class ProductList extends Component {
 
     if (!products.length) return <div>No products found</div>;
     return (
-      <div className="container">
+      <div className="productList">
         <h1>Featured Artists</h1>
         <div className="row">
-          <div className="aProduct">
-            {products.map(product => (
-              <div className=" col-xs-4" key={product.id}>
-              <Link
-                to={`products/${product.id}`}
-                key={product.id}
-                className="productCard"
-              >
-                <ProductItem theProduct={product} />
-              </Link>
-              </div>
-            ))}
+
+            <div className="aProduct">
+              {products.map(product => (
+                <Link
+                  to={`products/${product.id}`}
+                  key={product.id}
+                  className="productCard"
+                >
+                  <ProductItem theProduct={product} />
+                </Link>
+              ))}
+
           </div>
         </div>
         {isAdmin && <NewProductForm />}
@@ -76,6 +39,8 @@ class ProductList extends Component {
     );
   }
 }
+
+
 
 const mapState = state => ({
   currentUser: state.user,
@@ -89,3 +54,95 @@ const mapDispatch = dispatch => ({
 });
 
 export default connect(mapState, mapDispatch)(ProductList);
+
+// import React, { Component } from "react";
+// import { connect } from "react-redux";
+// import { Link } from "react-router-dom";
+// import { allProducts } from "../store";
+// import RaisedButton from "material-ui/RaisedButton";
+// import FontIcon from "material-ui/FontIcon";
+// import {
+//   Card,
+//   CardActions,
+//   CardHeader,
+//   CardMedia,
+//   CardTitle,
+//   CardText
+// } from "material-ui/Card";
+// import ProductItem from "./productItem";
+// import NewProductForm from "./newProduct";
+
+// const styles = {
+//   button: {
+//     margin: 12
+//   },
+//   card: {
+//     maxWidth: 345
+//   },
+//   media: {
+//     height: 200
+//   },
+//   exampleImageInput: {
+//     cursor: "pointer",
+//     position: "absolute",
+//     top: 0,
+//     bottom: 0,
+//     right: 0,
+//     left: 0,
+//     width: "100%",
+//     opacity: 0
+//   },
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "space-around"
+//   }
+// };
+
+// class ProductList extends Component {
+//   componentDidMount() {
+//     this.props.getAllProducts();
+//   }
+
+//   render() {
+//     const { products, currentUser } = this.props;
+
+//     const isAdmin = !!currentUser.isAdmin;
+
+//     if (!products.length) return <div>No products found</div>;
+//     return (
+//       <div className="container">
+//         <h1>Featured Artists</h1>
+//         <div className="row">
+//           <div className="aProduct">
+//             {products.map(product => (
+//               <div className=" col-xs-4" key={product.id}>
+//               <Link
+//                 to={`products/${product.id}`}
+//                 key={product.id}
+//                 className="productCard"
+//               >
+//                 <ProductItem theProduct={product} />
+//               </Link>
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//         {isAdmin && <NewProductForm />}
+//       </div>
+//     );
+//   }
+// }
+
+// const mapState = state => ({
+//   currentUser: state.user,
+//   products: state.products
+// });
+
+// const mapDispatch = dispatch => ({
+//   getAllProducts: () => {
+//     dispatch(allProducts());
+//   }
+// });
+
+// export default connect(mapState, mapDispatch)(ProductList);
