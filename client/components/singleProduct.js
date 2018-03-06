43bd6371b1cf86966e1367553c798b4d40bf8ae6
Roxie Turner <roxie.turner@gmail.com>
2018-03-06 14:44:53 -0500

    Working on logged in cart update behavior

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
 import { updateCartThunk } from '../store/cart';
 import  EditProductForm   from './editProduct';
+import { updateOrderLineThunk } from '../store/orderLine';
 
 
 // probably needs to be a stateful component?
@@ -18,43 +19,86 @@ class SingleProduct extends Component {
       imgUrl: ''
     };
   }
-  render() {
 
+  componentDidMount() {
+
+  }
+
+  render() {
     const { currentUser } = this.props;
     const product = this.props.singleProduct;
-    console.log('it is ', currentUser);
 
+    // console.log('it is ', currentUser.id);
+    // console.log('is logged in?', this.props.isLoggedIn);
+    // console.log('theOrder is: ', theOrder);
+
+    // if (this.props.isLoggedIn) {const theOrder = this.props.orders.id;}
     if (!product) return <div />;
     const isAdmin = !!currentUser.isAdmin
 
-    return (
-      <div>
-        <h2> Artist: {product.artist}</h2>
-        <h1>{product.title}</h1>
-        <img src={product.imgUrl} />
-        <h4>{product.description}</h4>
-        <h4>${product.price}</h4>
-        <h4>
-          <em>{product.quantity} available</em>
-        </h4>
-        <form onSubmit={this.props.handleAnonSubmit}>
-          <input
-            type="number"
-            name="quantity"
-            step="1"
-            defaultValue="1"
-            min="1"
-            max={product.quantity}
-          />
-          <button>Buy this Item</button>
-        </form>
-        <button>
-          <Link to="/products">Back to Products</Link>
-        </button>
-         {isAdmin && <EditProductForm history={this.props.history} product={product} />}
+    if (!this.props.isLoggedIn) {
+      return (
+        <div>
+          <h2> Artist: {product.artist}</h2>
+          <h1>{product.title}</h1>
+          <img src={product.imgUrl} />
+          <h4>{product.description}</h4>
+          <h4>${product.price}</h4>
+          <h4>
+            <em>{product.quantity} available</em>
+          </h4>
+            <form onSubmit={this.props.handleAnonSubmit}>
+              <input
+                type="number"
+                name="quantity"
+                step="1"
+                defaultValue="1"
+                min="1"
+                max={product.quantity}
+              />
+              <button>Buy this Item</button>
+            </form>
+          <button>
+            <Link to="/products">Back to Products</Link>
+          </button>
+          {isAdmin && <EditProductForm history={this.props.history} product={product} />}
 
-      </div>
-    );
+        </div>
+      );
+    } else if (this.props.isLoggedIn) {
+      return (
+        <div>
+          <h2> Artist: {product.artist}</h2>
+          <h1>{product.title}</h1>
+          <img src={product.imgUrl} />
+          <h4>{product.description}</h4>
+          <h4>${product.price}</h4>
+          <h4>
+            <em>{product.quantity} available</em>
+          </h4>
+          {this.props.orders &&
+            <form onSubmit={this.props.handleLoggedInSubmit}>
+              <input type="number" name="orderLineId" value={this.props.orders.orderlines[0].id} readOnly />
+              <input type="number" name="orderId" value={this.props.orders.id} readOnly />
+              <input
+                type="number"
+                name="quantity"
+                step="1"
+                defaultValue="1"
+                min="1"
+                max={product.quantity}
+              />
+              <button>Buy this Item</button>
+              </form>
+          }
+          <button>
+            <Link to="/products">Back to Products</Link>
+          </button>
+          {isAdmin && <EditProductForm history={this.props.history} product={product} />}
+
+        </div>
+      );
+    }
   }
 }
 
@@ -64,7 +108,8 @@ const mapState = function(state, ownProps) {
     singleProduct: state.products.find(
       product => product.id === +ownProps.match.params.id
     ),
-    currentUser: state.user
+    currentUser: state.user,
+    orders: state.orders[0]
   };
 };
 
@@ -74,7 +119,6 @@ const mapDispatch = function(dispatch, ownProps) {
     handleAnonSubmit(evt) {
       evt.preventDefault();
       const newId = ownProps.match.params.id;
-      console.log(ownProps.match.params.id);
       const orderLine = {
         [newId]: +evt.target.quantity.value
       };
@@ -83,13 +127,31 @@ const mapDispatch = function(dispatch, ownProps) {
 
     handleLoggedInSubmit(evt) {
       evt.preventDefault();
+      const productId = +ownProps.match.params.id;
+      const orderId = +evt.target.orderId.value;
+      const orderLineId = +evt.target.orderLineId.value;
+      // console.log(orderId, ' and ', productId);
 
-      const newId = ownProps.match.params.id;
-      // console.log(ownProps.match.params.id);
-      // const orderLine = {
-      //   [newId]: +evt.target.quantity.value
-      // };
-      dispatch(updateCartThunk(orderLine));
+      const updatedOrderLine = {
+        id: orderLineId,
+        quantity: +evt.target.quantity.value,
+        orderId: orderId,
+        productId: productId
+      }
+
+      console.log(updatedOrderLine);
+
+      updateOrderLineThunk(updatedOrderLine);
+
+      //update orderline array on orders....
+      //call updateOrderThunk with --- maybe not
+
+      //get orderID from state
+      //find orderline where orderID and productID from form match
+      // ....
+      //call update orderLineThunk with the correct quantity, orderID, productID
+
+
     }
   };
 };
