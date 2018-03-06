603859a1ffceaf9966d8422815ce5b5e0a425feb
johanna fulghum <johanna.fulghum@gmail.com>
2018-03-06 15:46:23 -0500

    Added Exact path

--- client/routes.js
+++ client/routes.js
@@ -34,7 +34,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/" component={SplashPage} />
+        <Route exact path="/" component={SplashPage} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route exact path="/products" component={ProductList} />
